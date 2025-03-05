234237bdf070c83e47f0d016d42d0c55208360e3
Ottoelif <ianchen611@gmail.com>
2025-03-05 22:53:46 +0800

    edit Tile.cpp

--- src/Tile.cpp
+++ src/Tile.cpp
@@ -1,8 +1,11 @@
 #include "Tile.hpp"
 #include "Util/Image.hpp"
+#include "Util/Logger.hpp"
 
 Tile::Tile(const std::string& ImagePath) {
+    LOG_INFO("Tile built");
     SetImage(ImagePath);
+    LOG_INFO("Tile's image already set.");
 }
 
 void Tile::SetImage(const std::string& ImagePath) {
