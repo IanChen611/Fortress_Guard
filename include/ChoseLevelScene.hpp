4a8082c6fde707e1d2d3dc7cd0d54eaf289f5e80
Ian <ianchen611@gmail.cim>
2025-03-10 08:22:03 +0000

    Trying and not complete.

--- include/ChoseLevelScene.hpp
+++ include/ChoseLevelScene.hpp
@@ -1,20 +1,29 @@
-// #ifndef CHOSELEVELSCENE_HPP
-// #define CHOSELEVELSCENE_HPP
+#ifndef CHOSELEVELSCENE_HPP
+#define CHOSELEVELSCENE_HPP
 
-// #include "Scene.hpp"
-// #include "Tile.hpp"
-// #include "Button.hpp"
-// #include "SceneManager.hpp"
+#include "Scene.hpp"
+#include "Tile.hpp"
+#include "Button.hpp"
+#include "SceneManager.hpp"
 
-// #include "Util/Logger.hpp"
-// #include "Util/Input.hpp"
-
-
-// class ChoseLevelScene : public Scene {
-// public:
+#include "Util/Logger.hpp"
+#include "Util/Input.hpp"
 
 
+class ChoseLevelScene : public Scene {
+public:
+    ChoseLevelScene(){
+        for(int i=1;i<=15;i++){
+            vec_Button.push_back(std::make_share<Button>(RESOURCE_DIR"/Image/UI/" + i.to_string() + ".png"), 48, 48, 1, 1, );
+        }
+        
+    }
 
 
-// };
-// #endif
+private:
+    std::vector<std::shared_ptr<Button>> vec_Button;
+};
+
+
+
+#endif
