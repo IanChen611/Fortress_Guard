bd91dced7d6e4900566c4bb96f6131fcc1d4e90d
Ottoelif <ianchen611@gmail.com>
2025-03-11 20:40:26 +0800

    file.cmake

--- files.cmake
+++ files.cmake
@@ -1,9 +1,26 @@
 set(SRC_FILES
     App.cpp
+    Button.cpp
+    ChooseLevelScene.cpp
+    Enemy.cpp
+    Level1.cpp
+    main.cpp
+    SceneManager.cpp
+    StartScene.cpp
+    Tile.cpp
+    TileSet.cpp
 )
 
 set(INCLUDE_FILES
     App.hpp
+    Button.hpp
+    ChooseLevelScene.hpp
+    Enemy.hpp
+    Level.hpp
+    SceneManager.hpp
+    StartScene.hpp
+    Tile.hpp
+    TileSet.hpp
 )
 
 set(TEST_FILES
